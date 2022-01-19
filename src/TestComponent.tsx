f5206c0dd215611abd0b933aa6aea872f52e2e70
hoshico <melancholinista.yasu@gmail.com>
2022-01-20 07:43:55 +0900

    add inputEvent

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -13,13 +13,22 @@ interface UserData {
 const TestComponent: React.FC<Props> = (props) => {
   // 初期値を0に設定した場合はnumber型と推論してくれる
   const [count, setCount] = useState<number | null>(null);
-
   const [user, setUser] = useState<UserData>({id: 1, name: "dummy"})
+  const [inputData, setInputData] = useState("")
   
+  // innputされた値(event.target)を引数として受け取る際
+  // 引数の型を定義する必要がある
+  // 下のonChangeをhoverすると引数の型を確認することができる
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData(e.target.value)
+  }
+
   return (
     <div>
       <h1>{props.text}</h1>
       <h1>{count}</h1>
+      <input type="text" value={inputData} onChange={handleInputChange} />
+      <h1>{inputData}</h1>
     </div>
   )
 }
