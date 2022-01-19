461ec0b3063d6b4a8b7b75e7b4df0203bd983366
hoshico <melancholinista.yasu@gmail.com>
2022-01-20 07:30:48 +0900

    add useState

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from "react"
 
 interface Props {
   text: string
 }
 
+// useStateにオブジェクトを使用したい場合はジェネリクスを使用する
+interface UserData {
+  id: number;
+  name: string
+}
+
 const TestComponent: React.FC<Props> = (props) => {
+  // 初期値を0に設定した場合はnumber型と推論してくれる
+  const [count, setCount] = useState<number | null>(null);
+
+  const [user, setUser] = useState<UserData>({id: 1, name: "dummy"})
+  
   return (
     <div>
       <h1>{props.text}</h1>
+      <h1>{count}</h1>
     </div>
   )
 }
